7d53200761ff48994e6986bfaf4fd105b82880da
Mike Frigger <mikefrigger1@gmail.com>
2025-09-09 13:29:04 +0800

    charset

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/clothe-donations-icon.png" />
+        <link rel="shortcut icon" href="/clothe-donations-icon.png" />
+        <link rel="apple-touch-icon" href="/clothe-donations-icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
